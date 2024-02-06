a81f3be778514c685c4c51bfdbde246eba329241
Ryan McIntyre <rmcintyre598@gmail.com>
2024-02-05 22:18:36 -0500

    added the tables structures

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,12 +1,32 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const { databaseVersion } = require('../../../UCF-VIRT-FSF-FT-12-2023-U-LOLC/13-ORM/01-Activities/24-Stu_One-to-Many/Solved/config/connection');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    }
   },
   {
     sequelize,
