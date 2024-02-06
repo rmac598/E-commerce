c85550584ca494b976705bde1990cdb3227e161e
Ryan McIntyre <rmcintyre598@gmail.com>
2024-02-06 08:04:43 -0500

    added one to many relationships

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -3,14 +3,30 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
-
-// Products belongsTo Category
-
 // Categories have many Products
 
-// Products belongToMany Tags (through ProductTag)
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
+
+// Tags hasMany Products (through ProductTag)
+Tag.hasMany(ProductTag, {
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE',
+});
+
+
+// Products belongToMany Tags (through ProductTag)
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'Tag_id',
+});
 
 module.exports = {
   Product,
